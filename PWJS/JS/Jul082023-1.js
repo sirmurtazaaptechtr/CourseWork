7260979b640827487dea69bdafa0dc159c433df0
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2023-07-08 18:53:44 +0500

    08-Jul-2023

---
+++ PWJS/JS/Jul082023-1.js
@@ -0,0 +1,97 @@
+//JavaScript Comparison/Relational Operators
+let x = 12;
+let y = 6;
+
+console.log(x>y);
+console.log(y>x);
+console.log(x<y);
+console.log(y<x);
+y = 12;
+console.log(x>=y);
+console.log(y>=x);
+console.log(x<=y);
+console.log(y<=x);
+
+y="12";
+
+console.log(x==y);
+console.log(x!=y);
+
+console.log(x===y);
+console.log(x!==y);
+//JavaScript Logical Operators
+
+
+let a = true;
+let b = true;
+console.log(a && b);
+
+a = true; b = false;
+console.log(a && b);
+
+a = false; b = true;
+console.log(a && b);
+
+a = false; b = false;
+console.log(a && b);
+
+a = true; b = true;
+console.log(a || b);
+
+a = true; b = false;
+console.log(a || b);
+
+a = false; b = true;
+console.log(a || b);
+
+a = false; b = false;
+console.log(a || b);
+
+a = true;
+console.log(!a);
+
+a = false;
+console.log(!a);
+
+let dt = 15;
+console.log(typeof(dt));
+
+// dt = new Date();
+// console.log(dt instanceof Date);
+// console.log(dt instanceof Number);
+
+console.log(5**2);
+console.log(2**3);
+
+let myVar;
+
+console.log(myVar);
+console.log(typeof(myVar));
+
+myVar = 17;
+
+console.log(myVar);
+console.log(typeof(myVar));
+
+myVar = "Ali Raza";
+
+console.log(myVar);
+console.log(typeof(myVar));
+
+x = "12";
+y = "2";
+
+console.log(x+y);
+
+let arr = ["Ali",28,"Sara",17];
+
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[1]);
+
+let obj = [{name:"Ali",age:28},{name:"Sara",age:17}];
+console.log(obj);
+console.log(obj[1].name);
+console.log(obj[1].age);
+
+
