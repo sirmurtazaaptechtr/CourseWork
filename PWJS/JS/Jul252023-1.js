2b1c8ba29c67d381b1b57324d72cb3d5b6dc4296
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2023-08-01 18:10:02 +0500

    Student App Complete

--- PWJS/JS/Jul252023-1.js
+++ PWJS/JS/Jul252023-1.js
@@ -10,7 +10,7 @@ function displayStudents() {
         students = JSON.parse(localStorage.getItem("students"));
         showContainer.innerHTML = '';
         students.forEach(function (e, i) {
-            showContainer.innerHTML += `<p>${i + 1}. Dear ${e.name} you are ${e.age} years old</p>`;
+            showContainer.innerHTML += `<p>${i + 1}. Dear ${e.name} you are ${e.age} years old. <input type="button" id="${i}" value="Delete" onclick="deleteNote(this.id)"></p>`;
         });
     }
 }
@@ -21,11 +21,20 @@ saveBtn.addEventListener("click", function () {
         students = [];
     } else {
         students = JSON.parse(localStorage.getItem("students"));
-        students.push({ name: nameBox.value, age: ageBox.value });
-        showContainer.innerHTML = '';
-        students.forEach(function (e, i) {
-            showContainer.innerHTML += `<p>${i + 1}. Dear ${e.name} you are ${e.age} years old</p>`;
-        });
+        students.push({ name: nameBox.value, age: ageBox.value });     
         localStorage.setItem("students", JSON.stringify(students));
+        displayStudents();
     }
 });
+function deleteNote(index){    
+    const previousData = localStorage.getItem("students");
+    if (previousData == null) {
+        students = [];
+    } else {
+        students = JSON.parse(localStorage.getItem("students"));
+        students.splice(index,1);
+        localStorage.setItem("students", JSON.stringify(students));
+        displayStudents();
+    }
+
+}
